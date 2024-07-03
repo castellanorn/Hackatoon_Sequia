443d5fedee19f032ad6582b8f226b4671645e110
jess-ar <jessica18_24@hotmail.com>
2024-07-04 01:07:40 +0200

    Add FormInput component, page ContactForm, and responsive design

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,30 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
-/*import Home from "../home";*/
+import Home from "../home";
 import Layout from "../components/layout/Layout";
+import ContactForm from "../pages/ContactForm";
 
+// Comentarios fuera del objeto de rutas
+// Rutas comentadas para futuras implementaciones:
+// const rutasComentadas = [
+//   { path: "/zonas", element: <ZonasAfectadas /> },
+//   { path: "/mapa", element: <Mapa /> },
+//   { path: "/user", element: <User /> },
+// ];
 
-export const router = createBrowserRouter ([
+export const router = createBrowserRouter([
     {
-        path:"/",
-        element:<Layout />,
-        /*children: [
+        path: "/",
+        element: <Layout />,
+        children: [
             {
                 path: "/",
-                element:<Home />
+                element: <Home />,
             },
             {
-                path: "/zonas",
-                element:<ZonasAfectadas />
+                path: "/contact",
+                element: <ContactForm />,
             },
-            {
-                path: "/mapa",
-                element:<Mapa />
-            },
-            {
-                path: "/user",
-                element:<User />
-            }
-        ]*/
+            
+        ],
     },
-])
+]);
+
 export default router;
