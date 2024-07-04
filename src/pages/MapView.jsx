6b9a337ca0369fe483829f8f6058802fe16da205
MegaDraconius <ivanvclinetor@gmail.com>
2024-07-05 00:38:55 +0200

    Update the map to obtain a view

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -1,42 +1,39 @@
-import 'leaflet/dist/leaflet.css';
-import { useState } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-const municipios = [
-  { nombre: "Barcelona", lat: 41.3851, lng: 2.1734 },
-  { nombre: "Girona", lat: 41.9794, lng: 2.8214 },
-  { nombre: "Lleida", lat: 41.6176, lng: 0.6200 },
-  { nombre: "Tarragona", lat: 41.1189, lng: 1.2445 },
-  // Agrega más municipios según sea necesario
-];
+// Corregir la ruta de los íconos de Leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+});
 
-const Mapa = () => {
-  const [posicion, setPosicion] = useState({ lat: 41.725559, lng: 1.827794 });
+const position = [41.385662, 2.167975];
 
-  const manejarCambioMunicipio = (event) => {
-    const municipioSeleccionado = municipios.find(m => m.nombre === event.target.value);
-    setPosicion({ lat: municipioSeleccionado.lat, lng: municipioSeleccionado.lng });
-  };
-
+const MapView = () => {
   return (
-    <div className='flex-col items-center'>
-      <select onChange={manejarCambioMunicipio} className='mb-2'>
-        {municipios.map((municipio) => (
-          <option key={municipio.nombre} value={municipio.nombre}>
-            {municipio.nombre}
-          </option>
-        ))}
-      </select>
-      <div className="">
-        <MapContainer center={[posicion.lat, posicion.lng]} zoom={8} className='h-4/5 w-4/5'>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer>
-      </div>
+    <div style={{ height: "75vh", width: "75%" }}>
+      <MapContainer
+        center={position}
+        zoom={8}
+        scrollWheelZoom={false}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
-}
+};
 
-export default Mapa;
+export default MapView;
