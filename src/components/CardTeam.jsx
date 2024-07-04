91a75c7209c138a96828b25f68a5dbe6f6e97437
maicodedev@gmail.com <maicodedev@gmail.com>
2024-07-04 14:05:47 +0200

    built-in tailwind

--- src/components/CardTeam.jsx
+++ src/components/CardTeam.jsx
@@ -1,17 +1,23 @@
+import PropTypes from 'prop-types';
 
-
-const CardTeam = (image, name, roll, description) => {
-  return (
-    <div className="team-member">
-    <img src= {image} alt={name} className="team-image" />
-    <h2>{name}</h2>
-    <h3>{roll}</h3>
-    <p>{description}</p>
+const CardTeam = ({ image, name, roll, description }) => {
+    return (
+        <div className="card bg-blue-700 mb-4 rounded-lg p-4">
+            <img src={image} alt={name} className="team-image rounded-full w-32 h-32 mx-auto mb-4" />
+            <h2 className="text-xl font-bold mb-2">{name}</h2>
+                <h3 className="text-lg mb-2">{roll}</h3>
+                    <p className="text-sm">{description}</p>
     </div>
-  )
-}
+
+    
+    );
+};
+
+CardTeam.propTypes = {
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    roll: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+};
 
 export default CardTeam;
-
-
-
--- src/pages/team/Team.jsx
+++ src/pages/team/Team.jsx
@@ -1,24 +1,22 @@
 
-import './Team.css';
-import CardTeam  from '../../components/CardTeam';
+import CardTeam from '../../components/CardTeam';
 
 const Team = () => {
     return (
-        <div className="text-center p-5">
-        <h1>Conoce a nuestro equipo</h1>
-        <p>
-        Nuestro equipo está formado por profesionales dedicados a brindarte el mejor servicio y apoyo en todos tus trámites y consultas.
-        Con una amplia experiencia y un compromiso firme con la atención ciudadana, estamos aquí para ayudarte en cada paso del camino.
+    <div className="bg-blue-800 text-white min-h-screen flex flex-col items-center justify-center p-5">
+        <h1 className="text-2xl font-bold mb-4">Conoce a nuestro equipo</h1>
+        <p className="text-center max-w-lg mb-8">
+        Nuestro equipo está formado por profesionales dedicados a brindarte el mejor servicio y apoyo en todos tus trámites y consultas. Con una amplia experiencia y un compromiso firme con la atención ciudadana, estamos aquí para ayudarte en cada paso del camino.
         </p>
 
-    <CardTeam name = 'Maite' image = '../../assets/images/maite.jpg' roll = 'Front-end dev' description = "Lorem ipsum bla bla bla" />
-    <CardTeam name = 'Jess' image = '../../assets/images/maite.jpg' roll = 'Front-end dev' description = "Lorem ipsum bla bla bla" />
-    <CardTeam name = 'Rosse' image = '../../assets/images/maite.jpg' roll = 'Front-end dev' description = "Lorem ipsum bla bla bla" />
-
+        <CardTeam name="Maite" image="../../assets/images/maite.jpg" roll="Desarrolladora Frontend" description="Lorem ipsum bla bla bla" />
+        <CardTeam name="Jess" image="../../assets/images/jess.jpg" roll="Desarrolladora Frontend" description="Lorem ipsum bla bla bla" />
+        <CardTeam name="Rosse" image="../../assets/images/rosse.jpg" roll="Desarrolladora Frontend" description="Lorem ipsum bla bla bla" />
     </div>
-);
+    );
 };
 
 export default Team;
+
 
 
