3696a7ca585ae8901eef45b615fa7712414d69e2
castellanorn <castellanorn@gmail.com>
2024-07-04 21:06:05 +0200

    add NavBar static with route ready

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,22 +1,21 @@
-
-import { NavLink, Link } from "react-router-dom/dist"
-
+//import { useState } from "react";
+import { HiBars3 } from "react-icons/hi2";
+import Menu from "../common/Menu"
 
 const NavBar = () => {
+  //const [menu, setMenu] = useState(false)
   return (
-  <header className="w-full h-20 mt- auto bg-navbar flex items-center justify-between">
-  <img src="../../assets/react.svg"></img>
-  <h1 className= "text-5xl logo-text">SEQUIATRON</h1>
-  <navbar className="flex gap-7 text-text text-3xl">
-    <Link>Inicio</Link>
-    <Link>Zonas Afectadas</Link>
-    <Link>Tips</Link>
-    <Link>Mapa</Link>
-    <Link>User</Link>
-  </navbar>
-  
-  
-  
+ 
+  <header className="w-auto h-20 px-4 py-2 mt-auto gap-1 bg-navbar flex justify-start items-center">
+    <img src="../../assets/images/favicon.ico.png" className="px-4 " >
+    </img>
+    <h1 className= "text-5xl logo-text font-serif">SEQUIATRON</h1>
+    <menu className="text-white scale-150 items-center px-4 lg:hidden" >
+      <HiBars3  />
+    </menu>
+    <navbar className="flex gap-7 px-4 mx-4 text-text text-xl hidden lg:flex lg:justify-end">
+      <Menu />
+    </navbar>
   </header>
   )
 }
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,13 +2,10 @@ import { Outlet } from "react-router-dom";
 import Footer from "../common/Footer";
 import NavBar from "../common/NavBar";
 
-
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-
         <NavBar />
-
       <div className="flex flex-grow">
         <main className="bg-[color:var(--col-components-bg)] flex flex-col w-full items-center py-5 overflow-y-auto">
           <Outlet />
