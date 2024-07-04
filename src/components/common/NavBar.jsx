6314602fd763529349b618c658a973c69bd35e94
castellanorn <castellanorn@gmail.com>
2024-07-04 12:21:01 +0200

    add modif navbar

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,8 +1,24 @@
 
+import { NavLink, Link } from "react-router-dom/dist"
+
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+  <header className="w-full h-20 mt- auto bg-navbar flex items-center justify-between">
+  <img src="../../assets/react.svg"></img>
+  <h1 className= "text-5xl logo-text">SEQUIATRON</h1>
+  <navbar className="flex gap-7 text-text text-3xl">
+    <Link>Inicio</Link>
+    <Link>Zonas Afectadas</Link>
+    <Link>Tips</Link>
+    <Link>Mapa</Link>
+    <Link>User</Link>
+  </navbar>
+  
+  
+  
+  </header>
   )
 }
 
-export default NavBar;
+export default NavBar
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../common/Footer";
-
+import NavBar from "../common/NavBar";
 
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header>
-        
-      </header>
+
+        <NavBar />
+
       <div className="flex flex-grow">
         <main className="bg-[color:var(--col-components-bg)] flex flex-col w-full items-center py-5 overflow-y-auto">
           <Outlet />
