d6972dba3e0366cc6d4f98d4c89ec1c7178c23cd
castellanorn <castellanorn@gmail.com>
2024-07-04 23:33:30 +0200

    edit router

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../home";
 import Layout from "../components/layout/Layout";
+import Zonas from "../pages/Zonas";
+import Mapa from "../pages/Mapa";
+import User from "../pages/User";
+import Tips from "../pages/Tips";
 import Team from "../pages/team/Team";
 import ContactForm from "../pages/ContactForm";
 
@@ -18,6 +22,10 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
         { path: "/", element: <Home /> },
+        { path: "/", element: <Zonas /> },
+        { path: "/", element: <Mapa /> },        
+        { path: "/user", element: <User /> },
+        { path: "/tips", element: <Tips /> },        
         { path: "/team", element: <Team /> },
         { path: "/contact", element: <ContactForm /> },
     
